cee1e4e3598238ab635ecf5d26353b4b64cb9590
Tim Bentley <tbentley@healthfirst.org>
2023-01-17 20:58:55 +0000

    Added s3 bucket module1

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3-bucket" {
   source  = "app.terraform.io/tbentley-training/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket_prefix = "tim bentley"
+  bucket_prefix = "tim-bentley"
   acl    = "private"
 
   versioning = {
